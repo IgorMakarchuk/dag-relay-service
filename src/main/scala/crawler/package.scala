85c90911ac241a02d700aae8ea9262cae4c6bf58
igor.makarchuk <igor.makarchuk@pimpay.ru>
2020-11-05 15:47:47 +0300

    add crawler by Makarchuk

--- src/main/scala/crawler/package.scala
+++ src/main/scala/crawler/package.scala
@@ -1,8 +1,17 @@
+import crawler.modules.CrawlerLive
 import domain._
-import zio.Task
+
+import zio.{Task, _}
 
 package object crawler {
+  type Crawler = Has[Service]
+
   trait Service {
     def fetch(project:Project):Task[Map[String, Dag]]
   }
+
+  val live: ZLayer[Any, Nothing, Crawler] = ZLayer.succeed(new CrawlerLive)
+
+//  // helpers
+//  def fetch: URIO[Config, AppConf] = ZIO.accessM[Config](_.get.app)
 }
